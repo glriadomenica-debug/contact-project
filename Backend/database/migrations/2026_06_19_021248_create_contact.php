ad1409c19c0d4f7b5d84a8f2fcf0dc54ae591df0
glriadomenica-debug <glriadomenica@gmail.com>
2026-06-19 09:43:26 +0700

    update

--- Backend/database/migrations/2026_06_19_021248_create_contact.php
+++ Backend/database/migrations/2026_06_19_021248_create_contact.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('contact', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
-            $table->string('last_name')->nullable();
-            $table->string('email')->nullable();
+            $table->string('last_name');
+            $table->string('email')->unique();
             $table->string('phone');
             $table->text('address');
             $table->date('birth_date')->nullable();
