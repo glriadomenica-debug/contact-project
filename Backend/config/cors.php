493ffd166ff8392e9ffacf6100919615b7a02eb3
glriadomenica-debug <glriadomenica@gmail.com>
2026-06-20 04:24:55 +0700

    update

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://contact-project-kappa.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
