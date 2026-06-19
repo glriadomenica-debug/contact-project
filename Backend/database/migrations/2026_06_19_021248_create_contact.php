f8de67ae670fd1264f49d98d54639b51797126f2
glriadomenica-debug <glriadomenica@gmail.com>
2026-06-19 09:31:25 +0700

    create contact migration

--- Backend/database/migrations/2026_06_19_021248_create_contact.php
+++ Backend/database/migrations/2026_06_19_021248_create_contact.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('contact', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone');
+            $table->text('address');
+            $table->date('birth_date')->nullable();
             $table->timestamps();
         });
     }
